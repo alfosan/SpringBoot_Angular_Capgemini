301c1475d018ce794d14a2718eea808983bc5c24
Llorenç Alfonso Sanchis <lloalfsan@gmail.com>
2025-02-28 14:43:26 +0100

    Data base and config --> SpringBoot

---
+++ backend/SpringBoot/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO cliente (nombre) VALUES ('Llorenç Alfonso');
+INSERT INTO cliente (nombre) VALUES ('Joan Ramirez');
+INSERT INTO cliente (nombre) VALUES ('Marta Sánchez');
+INSERT INTO cliente (nombre) VALUES ('Jordi Martínez');
+
+
+INSERT INTO game (titulo) VALUES ('Valorant');
+INSERT INTO game (titulo) VALUES ('League of Legends');
+INSERT INTO game (titulo) VALUES ('Fortnite');
+INSERT INTO game (titulo) VALUES ('Phasmophobia');
